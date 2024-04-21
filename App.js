a96c13d5ee0d069fb6dc4347a46333887964ea8b
GideonWeb3 <gideoncharlesweb3@gmail.com>
2024-04-22 02:50:55 +0300

    update

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { Switch, Text, View } from 'react-native';
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  console.log(colorScheme);
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center bg-red-500">
+      <View className="flex-row justify-center items-center space-x-2">
+        <Text className="text-xl">Toggle Theme</Text>
+        <Switch value={colorScheme} onChange={toggleColorScheme} />
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
